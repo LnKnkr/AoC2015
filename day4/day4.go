cdf9a9cc37856ac425a65f1438d29dfb7df9ab57
Ln <leon.kuenker@eddymax.com>
2022-11-21 15:27:47 +0100

    Add function for hash with seven leading zeros

--- day4/day4.go
+++ day4/day4.go
@@ -18,6 +18,9 @@ func main() {
 	fmt.Println("Day 4 - Part 2")
 	hash, index = getHash(file, getSixZeros)
 	fmt.Printf("%x\t%d\n", hash, index)
+
+	// hash, index = getHash(file, getSevenZeros)
+	// fmt.Printf("%x\t%d\n", hash, index)
 }
 
 // Since go had this already implemented i just needed to use it.
@@ -49,4 +52,14 @@ func getSixZeros(hashString string) bool {
 		hashString[3] == '0' &&
 		hashString[4] == '0' &&
 		hashString[5] == '0'
+}
+
+func getSevenZeros(hashString string) bool {
+	return hashString[0] == '0' &&
+		hashString[1] == '0' &&
+		hashString[2] == '0' &&
+		hashString[3] == '0' &&
+		hashString[4] == '0' &&
+		hashString[5] == '0' &&
+		hashString[6] == '0'
 }
